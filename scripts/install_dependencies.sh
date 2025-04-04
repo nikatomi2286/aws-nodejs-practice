7b399bd0b47b0c4206223e3fa52cf654805d68f8
ayeustsihneyeu <a.yeustsihneyeu@godeltech.com>
2025-04-04 14:07:39 +0200

    change install script 2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 # Install node.js npm
-#sudo yum update -y
-#sudo yum install -y nodejs npm
-
 sudo yum update -y
-sudo yum install -y curl
-curl -fsSL https://rpm.nodesource.com/setup_22.x | sudo bash -
-sudo yum install -y nodejs
-node -v
-npm -v
+sudo yum install -y nodejs npm
